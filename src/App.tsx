e35cdd6a97d277ee8a3c5d7e6c4f6299bce718bb
Ariel <ariel.arias01@epn.edu.ec>
2026-05-07 15:07:12 -0500

    Implementar la estructura principal de la aplicación con enlaces de navegación.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import ProductForm from "./components/ProductForm";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 p-10">
-      <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-xl p-8">
-        
-        <h1 className="text-4xl font-bold text-center mb-8">
-          CRUD Productos
-        </h1>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100 p-10">
 
-        <ProductForm
-          onCreated={() => {}}
-        />
+        <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-xl p-8">
+
+          <h1 className="text-4xl font-bold text-center mb-8">
+            Sistema CRUD
+          </h1>
+
+          {/* MENU */}
+          <div className="flex gap-4 justify-center mb-6">
+            <Link to="/products" className="text-blue-600">Productos</Link>
+            <Link to="/stores" className="text-blue-600">Tiendas</Link>
+            <Link to="/inventory" className="text-blue-600">Inventario</Link>
+          </div>
+
+          {/* RUTAS */}
+          <Routes>
+            <Route path="/products" element={<ProductForm onCreated={() => {}} />} />
+            <Route path="/stores" element={<div>Stores aquí</div>} />
+            <Route path="/inventory" element={<div>Inventory aquí</div>} />
+          </Routes>
+
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
