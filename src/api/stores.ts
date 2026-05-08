336c1116839d9698e8bfb322d273dfff31490d8f
Ariel <ariel.arias01@epn.edu.ec>
2026-05-08 12:38:26 -0500

    feat:actualización

--- src/api/stores.ts
+++ src/api/stores.ts
@@ -1,20 +1,32 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/store";
+const API_URL =
+  "http://localhost:3000/store";
 
+// GET
 export const getStores = () =>
   axios.get(API_URL);
 
-export const createStore = (data: any) =>
+// CREATE
+export const createStore = (
+  data: any
+) =>
   axios.post(API_URL, data);
 
+// UPDATE
 export const updateStore = (
   id: number,
   data: any
 ) =>
-  axios.patch(`${API_URL}/${id}`, data);
+  axios.put(
+    `${API_URL}/${id}`,
+    data
+  );
 
+// DELETE
 export const deleteStore = (
   id: number
 ) =>
-  axios.delete(`${API_URL}/${id}`);
+  axios.delete(
+    `${API_URL}/${id}`
+  );
