70b4f67db9eeb006932b0dd9cc0f63366d3a0faa
Ariel <ariel.arias01@epn.edu.ec>
2026-05-08 12:46:01 -0500

    feat: Cambio de el archivo inventory.ts

--- src/api/inventory.ts
+++ src/api/inventory.ts
@@ -3,24 +3,30 @@ import axios from "axios";
 const API_URL =
   "http://localhost:3000/inventory";
 
+// GET
 export const getInventory = () =>
   axios.get(API_URL);
 
+// CREATE
 export const createInventory = (
   data: any
 ) =>
   axios.post(API_URL, data);
 
+// UPDATE
 export const updateInventory = (
   id: number,
   data: any
 ) =>
-  axios.patch(
+  axios.put(
     `${API_URL}/${id}`,
     data
   );
 
+// DELETE
 export const deleteInventory = (
   id: number
 ) =>
-  axios.delete(`${API_URL}/${id}`);
+  axios.delete(
+    `${API_URL}/${id}`
+  );
