11a226db2c5633330a49653d046f2303fdecfbdb
Ariel <ariel.arias01@epn.edu.ec>
2026-05-13 09:08:20 -0500

    feat: crear página principal de reportes e integrar ReportsList

---
+++ src/pages/ReportsPage.tsx
@@ -0,0 +1,18 @@
+import ReportsList
+from "../components/reports/ReportsList";
+
+export default function ReportsPage() {
+
+  return (
+
+    <div>
+
+      <h1 className="text-4xl font-bold mb-8">
+        Módulo de Reportes
+      </h1>
+
+      <ReportsList />
+
+    </div>
+  );
+}
