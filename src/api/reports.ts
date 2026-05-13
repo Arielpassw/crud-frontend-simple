dadeddd394d739406aa2b3caa30f103f5496c0dc
Ariel <ariel.arias01@epn.edu.ec>
2026-05-13 09:01:13 -0500

    feat: agregar datos de reportes y ajustar diseño de lista de productos

---
+++ src/api/reports.ts
@@ -0,0 +1,30 @@
+export const reportsData = [
+
+  {
+    id: 1,
+    title: "Reporte Productos",
+    description:
+      "Listado general de productos",
+    file:
+      "/reports/productos.pdf",
+  },
+
+  {
+    id: 2,
+    title: "Reporte Tiendas",
+    description:
+      "Listado de tiendas registradas",
+    file:
+      "/reports/stores.pdf",
+  },
+
+  {
+    id: 3,
+    title: "Reporte Inventario",
+    description:
+      "Listado de inventario",
+    file:
+      "/reports/inventory.pdf",
+  },
+
+];
